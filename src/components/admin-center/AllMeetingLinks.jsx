5f308ec403c989809c395cfd74b2a7f00f2401db
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-04-13 13:17:17 +0530

    Update event flow

--- src/components/admin-center/AllMeetingLinks.jsx
+++ src/components/admin-center/AllMeetingLinks.jsx
@@ -15,10 +15,10 @@ const AllMeetingLinks = () => {
       <div className=" mt-5 px-2">
         <div className=" flex flex-col gap-2 mt-4">
           {defaultMeetLink && defaultMeetLink.mode ? (
-            <div className=" bg-gray-100   w-full flex gap-2 items-center justify-between px-2">
+            <div className=" bg-gray-100   w-full flex gap-2 items-center justify-between px-2 rounded-md">
               <div className=" flex flex-col font-poppins">
-                <p className=" p-2">{defaultMeetLink.mode}</p>
-                <p className=" p-2">{defaultMeetLink.meetingLink}</p>
+                <p className=" p-2 text-1xl  text-black">{defaultMeetLink.mode}</p>
+                <p className=" p-2 text-sm">{defaultMeetLink.meetingLink}</p>
               </div>
               <Trash2
                 className=" justify-end cursor-pointer text-red-400"
