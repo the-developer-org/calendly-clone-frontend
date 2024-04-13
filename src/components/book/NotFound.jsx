90de48ca652441d5dbe084abbaa033b9c01882fe
sourav0809 <spathak7431@gmail.com>
2024-04-13 11:36:54 +0530

    done

---
+++ src/components/book/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className=" w-screen h-screen flex justify-center items-center font-poppins text-2xl">
+      <h1>Selected Event is Not Found....</h1>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/routes/BookingRoutes.jsx
+++ src/routes/BookingRoutes.jsx
@@ -1,3 +1,4 @@
+import NotFound from '../components/book/NotFound';
 import Book from '../components/book/Book';
 import Message from '../components/book/Message';
 import { Routes, Route } from 'react-router-dom';
@@ -5,9 +6,10 @@ import { Routes, Route } from 'react-router-dom';
 const BookingRoutes = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <section className='w-full max-w-5xl p-8 bg-white shadow-lg rounded-lg'>
+      <section className="w-full max-w-5xl p-8 bg-white shadow-lg rounded-lg">
         <Routes>
-          <Route path='/book/:event-name/:id' element={<Book />} />
+          <Route path="/book/:event-name/:id" element={<Book />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </section>
     </div>
